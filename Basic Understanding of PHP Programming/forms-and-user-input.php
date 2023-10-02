535ca59806375058202e542f37237d07a92fbab5
John <emailjohnthomascaballero@gmail.com>
2023-10-02 10:47:02 +0800

    Day 4

---
+++ Basic Understanding of PHP Programming/forms-and-user-input.php
@@ -0,0 +1,9 @@
+<!-- Forms and User Input:
+Grasp the basics of processing data from HTML forms using PHP.
+Learn how to access form data through the $_POST and $_GET superglobal arrays. -->
+
+<?php 
+
+
+
+?>
