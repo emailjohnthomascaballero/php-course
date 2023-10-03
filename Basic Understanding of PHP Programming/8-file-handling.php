ad1a4f16657eb1a799414243039b68bd6074db7c
John <emailjohnthomascaballero@gmail.com>
2023-10-03 12:24:48 +0800

    Day 5

--- Basic Understanding of PHP Programming/8-file-handling.php
+++ Basic Understanding of PHP Programming/8-file-handling.php
@@ -8,7 +8,7 @@
 
 // 1. Open the File: Use the fopen() function to open the file. This function takes two arguments: the filename and the mode ("r" for reading).
 
-// $file = fopen("text.txt", "r");
+// $file = fopen("8-example.txt", "r");
 
 
 
@@ -26,4 +26,33 @@
 // $fclose($file);
 
 
+
+
+
+// Example of Reading and Writing a File:
+
+// Here's an example that reads the contents of a file named "8-example.txt" and writes the content to another file named "output.txt":
+
+// Open the input file for reading
+$inputFile = fopen("8-example.txt", "r");
+
+// Open the output file for writing (creates it if it doesn't exist)
+$outputFile = fopen("8-output.txt", "w");
+
+// Read and write line by line
+while ($line = fgets($inputFile)) {
+  fwrite($outputFile, $line);
+}
+
+// Close both files
+fclose($inputFile);
+fclose($outputFile);
+
+
+
 ?>
+
+
+<!-- Make sure to handle errors and exceptions when working with files, and always close the file when you're done to free up system resources.
+
+File handling is a crucial part of many PHP applications, from reading and writing configuration files to processing large datasets. It's essential to understand how to perform these operations securely and efficiently. -->
